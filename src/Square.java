bb1527854d6c3a2495d0519313529047cee12de4
kajal <kajalmalunjkar21@gmail.com>
2022-12-27 10:56:24 +0530

    commit message

---
+++ src/Square.java
@@ -0,0 +1,13 @@
+
+public class Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     float side=8;
+     double area=side * side;
+     double perimeter=4 * side;
+     
+     System.out.println("Area="+ area);
+     System.out.println("perimeter="+ perimeter);
+	}
+}
